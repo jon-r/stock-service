5268826c8fc6ed1fbb53a8e841afbc7b5301deb3
Jonathan Richards <jonathan.richards@ecs.co.uk>
2024-07-31 15:19:17 +0100

    data manager happy test

--- lambdas/cmd/data-manager/main_test.go
+++ lambdas/cmd/data-manager/main_test.go
@@ -4,16 +4,59 @@ import (
 	"context"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	dbTypes "github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	sqsTypes "github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/jon-r/stock-service/lambdas/internal/db"
+	"github.com/jon-r/stock-service/lambdas/internal/providers"
 	"github.com/jon-r/stock-service/lambdas/internal/testutil"
 )
 
-func TestHandleRequest(t *testing.T) {
-	t.Run("NoErrors", func(t *testing.T) { handleRequestNoErrors(t) })
+func TestUpdateAllTickers(t *testing.T) {
+	t.Run("NoErrors", updateAllTickerNoErrors)
 }
 
-func handleRequestNoErrors(t *testing.T) {
+func updateAllTickerNoErrors(t *testing.T) {
 	stubber, mockServiceHandler := testutil.EnterTest(nil)
 	mockHandler := DataManagerHandler{*mockServiceHandler}
+
+	expectedTickers := []db.TickerItem{
+		{
+			StocksTableItem: db.StocksTableItem{Id: "T#AMZN", Sort: "T#AMZN"},
+			Provider:        providers.PolygonIo,
+		},
+		{
+			StocksTableItem: db.StocksTableItem{Id: "T#META", Sort: "T#META"},
+			Provider:        providers.PolygonIo,
+		},
+	}
+	expectedQuery := &dynamodb.ScanInput{
+		TableName: aws.String("DB_STOCKS_TABLE_NAME"),
+		ExpressionAttributeNames: map[string]string{
+			"#0": "SK",
+			"#1": "Provider",
+		},
+		ExpressionAttributeValues: map[string]dbTypes.AttributeValue{
+			":0": &dbTypes.AttributeValueMemberS{Value: "T#"},
+		},
+		FilterExpression:     aws.String("begins_with (#0, :0)"),
+		ProjectionExpression: aws.String("#0, #1"),
+	}
+	stubber.Add(testutil.StubDynamoDbScan(expectedQuery, expectedTickers, nil))
+
+	expectedQueueItems := []sqsTypes.SendMessageBatchRequestEntry{
+		{
+			Id:          aws.String("TEST_ID"),
+			MessageBody: aws.String(`{"JobId":"TEST_ID","Provider":"POLYGON_IO","Type":"UPDATE_PRICES","TickerId":"AMZN,META","Attempts":0}`),
+		},
+	}
+	stubber.Add(testutil.StubSqsSendMessageBatch("", expectedQueueItems, nil))
+
+	expectedRule := "EVENTBRIDGE_RULE_NAME"
+	stubber.Add(testutil.StubEventbridgeEnableRule(expectedRule, nil))
+	expectedLambda := "LAMBDA_TICKER_NAME"
+	stubber.Add(testutil.StubLambdaInvoke(expectedLambda, nil))
 
 	err := mockHandler.updateAllTickers(context.TODO())
 
--- lambdas/internal/testutil/dynamodb.go
+++ lambdas/internal/testutil/dynamodb.go
@@ -1,17 +1,48 @@
 package testutil
 
 import (
+	"reflect"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/awsdocs/aws-doc-sdk-examples/gov2/testtools"
 )
 
+// todo pass in any and marshal here (dry out the test?)
 func StubDynamoDbAddTicker(tableName string, item map[string]types.AttributeValue, raiseErr error) testtools.Stub {
 	return testtools.Stub{
 		OperationName: "PutItem",
 		Input:         &dynamodb.PutItemInput{Item: item, TableName: aws.String(tableName)},
 		Output:        &dynamodb.PutItemOutput{},
+		Error:         StubbedError(raiseErr),
+	}
+}
+
+func unpackArray(s any) []any {
+	v := reflect.ValueOf(s)
+	r := make([]any, v.Len())
+	for i := 0; i < v.Len(); i++ {
+		r[i] = v.Index(i).Interface()
+	}
+	return r
+}
+
+func StubDynamoDbScan(request *dynamodb.ScanInput, response interface{}, raiseErr error) testtools.Stub {
+	// query, _ := attributevalue.MarshalMap(request)
+	list := unpackArray(response)
+
+	var items []map[string]types.AttributeValue
+	for _, item := range list {
+		data, _ := attributevalue.MarshalMap(item)
+		items = append(items, data)
+	}
+
+	return testtools.Stub{
+		OperationName: "Scan",
+		Input:         request,
+		Output:        &dynamodb.ScanOutput{Items: items},
 		Error:         StubbedError(raiseErr),
 	}
 }
